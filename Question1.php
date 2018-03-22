ebc5d784be86515ec2ed0d5d659e14ecebecba77
SAINTIER Eva <eva.saintier@my-digital-school.org>
2018-03-22 16:16:07 +0100

    tri fichiers

--- Question1.php
+++ Question1.php
@@ -1,5 +1,12 @@
+<?php 
+	session_start();
+
+?>
+
+
 <!doctype html>
 <html>
+	<html>
 	<head>
 		<meta charset="utf-8">
 		<title>Question1</title>
@@ -13,51 +20,84 @@
 
 	<body>
 		<div class="page">
-			<?php include('header.php');?>
+			<?php 
+				include('header.php');
+			
+				//Script connexion à la base de données
+				include('pdo.php');
+			
+				//Requête pour la table question
+				$idQuestion = 1;
+				$query = "SELECT * FROM question WHERE idQuestion=:id LIMIT 0,1";
+				$statementQuestion = $connexion->prepare($query);
+				$statementQuestion -> bindValue(':id', $idQuestion);
+				$statementQuestion -> execute();
+			
+			?>
 
 			<div class="block-center">
 				<div class="inner">
 					<main class="block-main">
 						<section class="section group">
 							<article class="item title">
-								<h1 class="title">Question</h1>	
+								<h1 class="title">Question <?php echo $idQuestion;?></h1>	
 							</article>					
 							<article class="item forms">
-								<form action="Question2.php">
-									<div class="quest">
-										<p>Tes soirées, c'est plus :</p>
-									</div>
-									<div>
-									<p>
-										<input type="radio" id="Choice1"
-										required name="contact" value="event">
-										<label for="contactChoice1">Aller chill autour d'un verre </label><br>
-									</p>
-									<p>
-										<input type="radio" id="Choice2"
-										required name="contact" value="concert">
-										<label for="contactChoice2">Aller à un concert</label><br>
-									</p>
-									<p>
-										<input type="radio" id="Choice3"
-										required name="contact" value="theatre">
-										<label for="contactChoice3">Aller au théâtre</label><br>
-									</p>
-									</div>
-									<div>
-										<button class="btn" type="submit">1/3</button>
+								
+								<?php 
+								//Passage à la question suivante
+								if ($idQuestion == 1) {
+									echo "<form method='get' action='Question2.php'>";
 
+
+								//Si erreur, redirection vers début du formulaire
+								} else {
+									echo "Erreur, pour revenir au début du formulaire, <a href='debutformulaire.php'>cliquez-ici</a>";
+								}
+								?>
+								
+								
+									<div class="quest">
+										<?php
+										//Affichage de la question
+										while ($question = $statementQuestion -> fetch()) {
+										echo "<p>".$question -> texteQuestion."</p>";
+										?>
 									</div>
-										<input class="end" type="submit" value="Suivant">
+									<div>
+										<?php 	
+											//Requête pour la table réponse
+											$query = "SELECT * FROM reponse WHERE idQuestion=:id";
+											$statementReponse = $connexion->prepare($query);
+											$statementReponse -> bindValue(':id', $idQuestion);
+											$statementReponse -> execute();
+
+
+											//Affichage des réponses correspondantes
+											$i=0;
+											while ($reponse = $statementReponse -> fetch()) {
+												$i++;
+												echo "<p><input id='reponse".$i."' name='reponse1' type='radio' value ='".$reponse -> idReponse."'required><label for='reponse".$i."'>".$reponse -> texteReponse."</label></p>";		
+
+											} 
+
+										}
+										?>
+										<input class="button" name="submit" type="submit" value="Valider">
+									</div>
 								</form>
 							</article>
 						</section>				
 					</main>
 				</div>
 			</div>	
-		
-			
+
+
 			<?php include('footer.php');?>
 		</div>
 	</body>
 </html>
+
+			
+
+			
