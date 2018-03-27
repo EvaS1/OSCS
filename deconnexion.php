d802197f0e91fb0ae922ecb8571501667d5ee1d5
SAINTIER Eva <eva.saintier@my-digital-school.org>
2018-03-27 15:11:59 +0200

    style page déconnexion

--- deconnexion.php
+++ deconnexion.php
@@ -1,14 +1,41 @@
-<?php session_start();?>
+<?php session_start(); ?>
 <!doctype html>
 <html>
-<head>
-<meta charset="utf-8">
-<title>Document sans titre</title>
-</head>
+	<head>
+		<meta charset="utf-8">
+		<title>Où sortir ce soir ?</title>
+		<link rel="icon" href="Logo/logof.png">
+		<link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" href="vendors/bootstrap/css/bootstrap-grid.min.css">
+		<link rel="stylesheet" href="vendors/bootstrap/css/bootstrap.min.css">
+		<script src="jquery.min.js"></script>
+		<script src="menu.js"></script>
+		
+	</head>
 
-<body>
-<?php session_destroy(); 
-	echo "Tu as bien été déconnecté(e), pour te reconnecter <a href='connexion.php'>clique ici</a>.";?>
-
-</body>
+	<body>
+		<div class="page">
+			<?php include('header.php');
+			include('pdo.php');?>
+			<div class="content">
+				<main class="block-main-deconnexion">
+					<div class="container">
+						<div class="row">
+							<div class="col-12">
+								<div class="title">
+									<h2>Déconnexion</h2>
+								</div>
+								<?php session_destroy(); ?>
+								<div class='done'>
+									<p>Tu as été déconnecté(e), pour te reconnecter <a href='connexion.php'>clique ici</a>.</p>
+								</div>								 						
+							</div>
+						</div>
+					</div>
+				</main>
+			</div>
+			<?php include('footer.php');?>
+		</div>
+	</body>
+	
 </html>
